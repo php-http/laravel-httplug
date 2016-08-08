f28aea3584a5d91648c76ec1e05ba0e0abaf119d
Mathieu Santo Stefano--Féron <msantostefano@jolicode.com>
2016-08-08 15:23:13 +0200

    Fix code style for default config file (#6)

--- config/laravel-httplug.php
+++ config/laravel-httplug.php
@@ -1,27 +1,24 @@
 <?php
- 
- 
+
 return [
- 
-    'default'   => 'guzzle6',
- 
- 
+
+    'default' => 'guzzle6',
+
     'adapters' => [
- 
+
         /**
          * @link https://github.com/php-http/guzzle6-adapter
          */
         'guzzle6' => [
             'factory' => 'httplug.factory.guzzle6',
             'plugins' => [
-                    'httplug.plugin.authentication.my_wsse',
-                    'httplug.plugin.cache',
-                    'httplug.plugin.retry',
-
+                'httplug.plugin.authentication.my_wsse',
+                'httplug.plugin.cache',
+                'httplug.plugin.retry',
             ],
- 
+
             'config' => [
-                'verify'  => false,
+                'verify' => false,
                 'timeout' => 2,
             ],
         ],
@@ -29,36 +26,36 @@ return [
         /**
          * @link https://github.com/php-http/guzzle5-adapter
          */
-        'guzzle5'   => [
+        'guzzle5' => [
 
         ],
 
         /**
          * @link https://github.com/php-http/curl-client
          */
-        'curl'  => [
+        'curl' => [
 
         ],
 
         /**
          * @link https://github.com/php-http/socket-client
          */
-        'socket'    => [
+        'socket' => [
 
         ],
 
         /**
          * @link https://github.com/php-http/buzz-adapter
          */
-        'buzz'   => [
-            'resolver'  => [
-              'timeout'     => 5,
-              'verify_peer' => true,
-              'verify_host' => 2,
-              'proxy'       => null,
+        'buzz' => [
+            'resolver' => [
+                'timeout' => 5,
+                'verify_peer' => true,
+                'verify_host' => 2,
+                'proxy' => null,
             ],
         ],
-        
+
         /**
          * @link https://github.com/php-http/react-adapter
          */
@@ -67,5 +64,5 @@ return [
         ],
 
     ],
- 
+
 ];
